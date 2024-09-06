c73d76e9d411e7cf870705bd6a30e351f55d4ec2
Daniel <vietn@kicmail.com>
2024-09-06 14:05:01 +0700

    server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,17 @@
 const app = require('./app.js');
-// const https = require('https');
-// const fs = require('fs');
+const https = require('https');
+const fs = require('fs');
 const port = process.env.PORT || 8081;
 
-// const options = {
-//   cert: fs.readFileSync('E:\\backend\\cert.pem'),
-//   key: fs.readFileSync('E:\\backend\\cert.key'),
-// };
+const options = {
+  cert: fs.readFileSync('D:\\socialinsurance\\backend\\cert.pem'),
+  key: fs.readFileSync('D:\\socialinsurance\\backend\\cert.key'),
+};
 
-// https.createServer(options, app).listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
-
-app.listen(port, () => {
+https.createServer(options, app).listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
