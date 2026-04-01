89d689284a17e5aab7a651c9222294f245364272
vietdan1613 <viet.dan.1613@gmail.com>
2026-04-01 16:07:25 +0700

    update chanhhung

--- frontend/src/services/mapper.js
+++ frontend/src/services/mapper.js
@@ -1,12 +1,14 @@
 const TENANT_CONFIG_URL = {
     xomchieu: 'https://xomchieu.bhxh-online.com/api',
     thuduc: 'https://thuduc.bhxh-online.com/api',
+    chanhhung: 'https://chanhhung.bhxh-online.com/api',
     default: 'https://api.example.com',
 }
 
 const TENANT_CONFIG_NAME = {
     xomchieu: 'XÓM CHIẾU',
     thuduc: 'THỦ ĐỨC',
+    chanhhung: 'CHÁNH HƯNG',
     default: '',
 }
 
